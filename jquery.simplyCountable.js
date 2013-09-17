4cca20d2e3a07fca1f8b89111300ea172af1fb97
blake <blakefromla@gmail.com>
2013-09-17 08:40:56 -0700

    Made it compatible with wordpress, which has the need to run JQeury in no conflict mode.

--- jquery.simplyCountable.js
+++ jquery.simplyCountable.js
@@ -10,8 +10,8 @@
 * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
 * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
 */
-
-(function($){
+jQuery.noConflict();
+jQuery(document).ready(function($){
 
   $.fn.simplyCountable = function(options){
     
@@ -134,4 +134,4 @@
     
   };
 
-})(jQuery);
+});
